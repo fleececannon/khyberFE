8488e640389bf222f9ff73d55fdbaf8fc2120fa1
fleececannon <121724981+fleececannon@users.noreply.github.com>
2023-02-05 18:45:00 -0800

    "Laptop Commit"

--- content-script.js
+++ content-script.js
@@ -5,16 +5,17 @@ document.addEventListener("click", (event) => {
   if (event.target.className === "oZ-jc T-Jo J-J5-Ji T-Jo-Jp") {
     console.log("Event target class name is oZ-jc T-Jo J-J5-Ji T-Jo-Jp");
     addSelectorToHeader();
+    getHoverColor();
   }
 });
 
 const getHoverColor = () => {
   // Retrieve the color of a relevant element
-  const relevantElement = document.querySelector(".G-asx T-I-J3 J-J5-Ji");
+  const relevantElement = document.querySelector(".aRz J-KU");
   console.log(relevantElement);
   const computedStyle = window.getComputedStyle(relevantElement);
   const hoverColor = computedStyle.getPropertyValue("background-color");
-
+  console.log(hoverColor);
   // Return the hover color
   return hoverColor;
 };
@@ -49,61 +50,59 @@ const createSelector = () => {
 
   // Create options container
   const optionsContainer = document.createElement("div");
-  optionsContainer.style.display = "none";
+  optionsContainer.style.display = "block";
+  optionsContainer.style.backgroundColor = "white";
+  optionsContainer.style.borderRadius = "4px";
+  optionsContainer.style.boxShadow = "0 2px 4px rgb(0 0 0 / 20%)";
 
   // Add options to the options container
   const option1 = document.createElement("div");
   option1.textContent = "Option 1";
+  option1.style.paddingBottom = "6px";
+  option1.style.paddingTop = "6px";
+  option1.style.color = "#202124";
+  option1.style.paddingLeft = "48px";
+  option1.style.paddingRight = "48px";
+  option1.style.font = "roboto";
+  option1.style.fontSize = ".875rem";
+  option1.style.fontFamily = "Roboto";
   optionsContainer.appendChild(option1);
 
   const option2 = document.createElement("div");
   option2.textContent = "Option 2";
+  option2.style.paddingBottom = "6px";
+  option2.style.paddingTop = "6px";
+  option2.style.color = "#202124";
+  option2.style.paddingLeft = "48px";
+  option2.style.paddingRight = "48px";
+  option2.style.font = "roboto";
+  option2.style.fontSize = ".875rem";
+  option2.style.fontFamily = "Roboto";
   optionsContainer.appendChild(option2);
+
+  const option3 = document.createElement("div");
+  option3.textContent = "Option 2";
+  option3.style.paddingBottom = "6px";
+  option3.style.paddingTop = "6px";
+  option3.style.color = "#202124";
+  option3.style.paddingLeft = "48px";
+  option3.style.paddingRight = "48px";
+  option3.style.font = "roboto";
+  option3.style.fontSize = ".875rem";
+  option3.style.fontFamily = "Roboto";
+  optionsContainer.appendChild(option3);
 
   selectorContainer.appendChild(button);
   selectorContainer.appendChild(optionsContainer);
 
   button.addEventListener("click", function () {
+    console.log("button click");
     optionsContainer.style.display =
       optionsContainer.style.display === "none" ? "block" : "none";
   });
 
   return selectorContainer;
 };
-
-// const createSelector = () => {
-//   // Create a div element
-//   const selectorContainer = document.createElement("div");
-//   selectorContainer.style.display = "inline-block";
-
-//   // Create an image element
-//   const khyberImage = document.createElement("img");
-//   khyberImage.src =
-//     "https://uploads-ssl.webflow.com/63d84672894ec400b3fa0cfe/63d87d54eea9124df8bf575f_color-p-500.png";
-//   khyberImage.style.height = "20px";
-//   khyberImage.style.width = "20px";
-//   selectorContainer.appendChild(khyberImage);
-
-//   // Create a dropdown selector
-//   const selector = document.createElement("select");
-//   selector.style.height = "20px";
-//   selector.style.width = "100px";
-//   selector.style.marginLeft = "5px";
-
-//   // Add options to the selector
-//   const option1 = document.createElement("option");
-//   option1.value = "option1";
-//   option1.text = "Option 1";
-//   selector.add(option1);
-
-//   const option2 = document.createElement("option");
-//   option2.value = "option2";
-//   option2.text = "Option 2";
-//   selector.add(option2);
-
-//   selectorContainer.appendChild(selector);
-//   return selectorContainer;
-// };
 
 const addSelectorToHeader = () => {
   // Get the Gmail inbox header element
